4620bced5016e1d016c2a0b8bad60119e0bdbf7c
root <root@localhost.localdomain>
2019-01-05 14:44:29 +0000

    Increasing scale and adding new initial stateto DLA

--- codebase/dla2.py
+++ codebase/dla2.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt, matplotlib.animation as animation
-import numpy as np, utility
+import numpy as np, utility, sys
 
 
 def create_start(padding, state, verbose):
@@ -22,10 +22,11 @@ def create_start(padding, state, verbose):
 def feed_steps_to_state(start, state, steps):
     history = []
     for step in steps:
-        x = start[0]
-        y = start[1]
-        state[x, y] = 0
-        directions = {1: [x-1, y-1],
+        try:
+            x = start[0]
+            y = start[1]
+            state[x, y] = 0
+            directions = {1: [x-1, y-1],
                       2: [x, y-1],
                       3: [x+1, y-1],
                       4: [x-1, y],
@@ -35,9 +36,9 @@ def feed_steps_to_state(start, state, steps):
                       8: [x, y+1],
                       9: [x+1, y+1]}
 
-        # If there's something already there,
-        # leave the particle before moving on
-        try:
+            # If there's something already there,
+            # leave the particle before moving on
+        
             next_step = state[directions[step][0],directions[step][1]]
         except IndexError:
             # Out of Bounds!
@@ -73,10 +74,14 @@ def splice_timeline(series1, series2):
 
 
 def main():
-    n_steps = 500
-    n_particles = 1000
+    n_steps = 220
+    n_particles = 1800
     dimensions = [250, 250]
-    center_mass_size = 3
+    checker = True
+    if '-cm' not in sys.argv:
+        center_mass_size = 5
+    else:
+        center_mass_size = int(sys.argv[2])
 
     # Numbering scheme for direction keys
     # [[1,2,3],
@@ -85,14 +90,56 @@ def main():
     state = np.zeros(dimensions)
     center_x = int(state.shape[0]) / 2
     center_y = int(state.shape[1]) / 2
-    # Put a box in the center
-    state[center_x - center_mass_size:center_x + center_mass_size,
-          center_y - center_mass_size:center_y + center_mass_size] = 1
+
+    if checker:
+        state[90:100, 50:60] = 1
+        state[90:100, 70:80] = 1
+        state[90:100, 90:100] = 1
+        state[90:100, 110:120] = 1
+        state[90:100, 130:140] = 1
+        state[90:100, 150:160] = 1
+        state[90:100, 170:180] = 1
+        state[90:100, 190:200] = 1
+
+        state[110:120, 30:40] = 1
+        state[110:120,50:60] = 1
+        state[110:120,70:80] = 1
+        state[110:120,90:100] = 1
+        state[110:120, 110:120] = 1
+        state[110:120, 130:140] = 1
+        state[110:120, 150:160] = 1
+        state[110:120, 170:180] = 1
+        state[110:120, 190:200] = 1
+        state[110:120, 210:220] = 1
+
+        state[130:140, 30:40] = 1
+        state[130:140, 50:60] = 1
+        state[130:140, 70:80] = 1
+        state[130:140, 90:100] = 1
+        state[130:140, 110:120] = 1
+        state[130:140, 130:140] = 1
+        state[130:140, 150:160] = 1
+        state[130:140, 170:180] = 1
+        state[130:140, 190:200] = 1
+        state[130:140, 210:220] = 1
+
+        state[150:160, 50:60] = 1
+        state[150:160, 70:80] = 1
+        state[150:160, 90:100] = 1
+        state[150:160, 110:120] = 1
+        state[150:160, 130:140] = 1
+        state[150:160, 150:160] = 1
+        state[150:160, 170:180] = 1
+        state[150:160, 190:200] = 1
+    else:
+        # Put a box in the center
+        state[center_x - center_mass_size:center_x + center_mass_size,
+        center_y - center_mass_size:center_y + center_mass_size] = 1
 
     data = []
 
     for particle in range(n_particles):
-        start = create_start(10, np.zeros((250, 250)), False)
+        start = create_start(10, np.zeros(dimensions), False)
         steps = np.random.randint(1, 9, n_steps)
 
         # print "Starting from "+str(start)
@@ -102,7 +149,8 @@ def main():
         state = frames.pop()
         data.append(np.array(state))
 
-    utility.ImageProcessing.render(data, False, 20, True, 'DLA_Universe.mp4')
+    print "Simulation completed. Rendering " + str(len(data)) + " Frames"
+    utility.ImageProcessing.render(data, False, 15, False, '')
 
 
 if __name__ == '__main__':
