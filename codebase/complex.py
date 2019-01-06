ab7e647b857e75375da24c004f0a93c0943786e7
root <root@localhost.localdomain>
2019-01-06 12:10:58 +0000

    Reorganizing Project

--- codebase/complex.py
+++ codebase/complex.py
@@ -13,7 +13,7 @@ class complex_movements:
 
     position = []
 
-    def __init__(self, dims, world, ratio, duration):
+    def __init__(self, dims, world, ratio, duration,opts):
         self.state = world
         self.mix = ratio
         self.length = duration
@@ -24,6 +24,8 @@ class complex_movements:
         self.state = draw_square_lattice(self.state)
         nrw, nau = self.initialize()
         self.simulate(n_random=nrw, n_auto=nau)
+        if 'swarm' in opts:
+            self.simulate_swarm(5, nrw)
 
     def make_box(self, width,height):
         center_x = width / 2
@@ -46,6 +48,63 @@ class complex_movements:
 
         return n_random, n_auto
 
+    def simulate_swarm(self, n_particles, n_random):
+        simulation = []
+        visual = []
+        f0 = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]  # Key #s: 9, 6, 3, 1
+        f1 = [[1, 2, 1], [2, 3, 2], [1, 2, 1]]  # Key #s: 15, 11, 8, 4
+        f2 = [[1, 1, 1, 1, 1], [1, 2, 2, 2, 1], [1, 2, 0, 2, 1], [1, 2, 2, 2, 1],
+              [1, 1, 1, 1, 1]]  # Key #s: 23, 16, 11, 8
+
+        automata_baselines = {'f0': [9, 6, 3, 1],
+                              'f1': [15, 11, 8, 4],
+                              'f2': [23, 16, 11, 8]}
+        t0 = time.time()
+        for frame in range(self.length):
+            for pt in range(n_particles):
+                # Initialize a random position
+                x = np.random.randint(0,self.width,1,dtype=int)
+                y = np.random.randint(0,self.height,1,dtype=int)
+                self.position = [x, y]
+                random_steps = np.random.random_integers(1, 9, n_random)
+                # Add random steps to simulation matrix
+                for step in random_steps:
+                    directions = {1: [self.position[0] - 1, self.position[1] - 1],
+                                  2: [self.position[0], self.position[1] - 1],
+                                  3: [self.position[0] + 1, self.position[1] - 1],
+                                  4: [self.position[0] - 1, self.position[1]],
+                                  5: [self.position[0], self.position[1]],
+                                  6: [self.position[0] + 1, self.position[1]],
+                                  7: [self.position[0] - 1, self.position[1] + 1],
+                                  8: [self.position[0], self.position[1] + 1],
+                                  9: [self.position[0] + 1, self.position[1] + 1]}
+                    try:
+                        self.position = directions[step]
+                        self.state[self.position[0], self.position[1]] += 1
+                        simulation.append(self.state)
+                        visual.append([plt.imshow(self.state, 'gray_r')])
+                    except IndexError:
+                        continue
+                # Add conv/automata to simulation matrix
+                world = ndi.convolve(self.state, f1)
+                flat_state = self.state.flatten()
+                II = 0
+                for cell in world.flatten():
+                    for keynum in automata_baselines['f1']:
+                        if cell == keynum:
+                            flat_state[II] -= 1
+                        if cell >= keynum:
+                            flat_state[II] -= 2
+                    II += 1
+                self.state = flat_state.reshape(self.width, self.height)
+                simulation.append(world)
+                visual.append([plt.imshow(self.state, 'gray_r')])
+        print "Finished Simulation [" + str(II) + " Frames in " + str(time.time() - t0) + 's'
+        a = animation.ArtistAnimation(plt.figure(), visual, interval=1, blit=True, repeat_delay=800)
+        # plt.imshow(world)
+        plt.show()
+        return simulation
+
     def simulate(self, n_random, n_auto):
         simulation = []
         visual = []
@@ -58,6 +117,8 @@ class complex_movements:
                               'f2': [23, 16, 11, 8]}
         t0 = time.time()
         for frame in range(self.length):
+            # Initialize a random position
+
             random_steps = np.random.random_integers(1,9,n_random)
             # Add random steps to simulation matrix
             for step in random_steps:
@@ -186,10 +247,13 @@ def main():
         walk_ratio = int(input('How Many Parts RandomWalk?: '))
         auto_ratio = int(input('How Many Parts Automata?: '))
 
-    # Based on either defaults, or user defined params, create a
-    # set of complex movements with a given mixture of random walk
-    #  motion and automata movements.
-    complex_movements([N, N], np.zeros((N, N)), [walk_ratio, auto_ratio], 100)
+    if 'swarm' in sys.argv:
+        complex_movements([N,N], np.zeros((N,N)), [walk_ratio, auto_ratio], 100,['swarm'])
+    else:
+        # Based on either defaults, or user defined params, create a
+        # set of complex movements with a given mixture of random walk
+        #  motion and automata movements.
+        complex_movements([N, N], np.zeros((N, N)), [walk_ratio, auto_ratio], 100,[''])
 
 
 if __name__ == '__main__':
