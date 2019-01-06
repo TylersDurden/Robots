e8180631956f0408b38d557060b7d80d3c3dafec
root <root@localhost.localdomain>
2019-01-06 01:20:15 +0000

    Synchronized DLA [SDLA] is working

--- README.md
+++ README.md
@@ -23,11 +23,22 @@ A second example with a different initial condition [5500 Points]:
 
 A video example of the process occuring [2200 Points]: 
 
-![dlaExVid](https://raw.githubusercontent.com/TylersDurden/Robots/master/dlaEx.mp4)
+[dlaExVid](https://raw.githubusercontent.com/TylersDurden/Robots/master/dlaEx.mp4)
 
 In dla2.py, particles are added on-screen sequentially (though their movements are precomputed 
 offscreen for improved performance), greatly adding to the length of frames in the simulation (each particle added is captured in a frame, i.e. 5000 particles means 5000 frames). 
 
 In this such DLA simulation, many particles can be initialized at once.The python program dla3.py
-will include this new scheme. 
-  
+includes this new scheme. 
+
+dla3 turns out to be significantly faster as well, considering how many
+more particles and step combinations are added. 
+
+![10KDLA](https://raw.githubusercontent.com/TylersDurden/Robots/master/10kDLA.png)
+
+Running points synchronously means less accumulate in the void from clouds of other dead points, and
+hopefully because the programs computation scales better, we should be able to actually *see* the growth
+of structures in a reasonable computation time with many more total points used. 
+
+![SDLA](https://raw.githubusercontent.com/TylersDurden/Robots/master/synchronized_dla.mp4)
+
--- codebase/dla3.py
+++ codebase/dla3.py
@@ -1,4 +1,5 @@
-import numpy as np, utility, sys
+import matplotlib.pyplot as plt, matplotlib.animation as animation
+import numpy as np, time, sys
 
 
 def create_start(padding, state, verbose):
@@ -56,8 +57,9 @@ class RandomWalker:
             self.pos = [next_pos[0], next_pos[1]]
         return steps
 
-    def run_synchronous_dla(self, point_cloud, initial_state):
+    def run_synchronous_dla(self, point_cloud, initial_state, show):
         simulation = []
+        visual = []
         for step in range(self.N):
             for pt in point_cloud.keys():
                 try:
@@ -71,15 +73,95 @@ class RandomWalker:
                 except IndexError:
                     break
             simulation.append(initial_state)
+            visual.append([plt.imshow(initial_state, 'gray_r')])
+
         print str(len(simulation)) + " Frames in simulation"
+        if show:
+            f = plt.figure(figsize=(8,8))
+            a = animation.ArtistAnimation(f,visual,interval=10,blit=True,repeat_delay=800)
+            plt.show()
+
         return simulation
+
+
+def draw_square_lattice(state):
+    state[30:40, 110:120] = 1
+    state[30:40, 130:140] = 1
+
+    state[50:60, 90:100] = 1
+    state[50:60, 110:120] = 1
+    state[50:60, 130:140] = 1
+    state[50:60, 150:160] = 1
+
+    state[70:80, 70:80] = 1
+    state[70:80, 90:100] = 1
+    state[70:80, 110:120] = 1
+    state[70:80, 130:140] = 1
+    state[70:80, 150:160] = 1
+    state[70:80, 170:180] = 1
+
+    state[90:100, 50:60] = 1
+    state[90:100, 70:80] = 1
+    state[90:100, 90:100] = 1
+    state[90:100, 110:120] = 1
+    state[90:100, 130:140] = 1
+    state[90:100, 150:160] = 1
+    state[90:100, 170:180] = 1
+    state[90:100, 190:200] = 1
+
+    state[110:120, 30:40] = 1
+    state[110:120, 50:60] = 1
+    state[110:120, 70:80] = 1
+    state[110:120, 90:100] = 1
+    state[110:120, 110:120] = 1
+    state[110:120, 130:140] = 1
+    state[110:120, 150:160] = 1
+    state[110:120, 170:180] = 1
+    state[110:120, 190:200] = 1
+    state[110:120, 210:220] = 1
+
+    state[130:140, 30:40] = 1
+    state[130:140, 50:60] = 1
+    state[130:140, 70:80] = 1
+    state[130:140, 90:100] = 1
+    state[130:140, 110:120] = 1
+    state[130:140, 130:140] = 1
+    state[130:140, 150:160] = 1
+    state[130:140, 170:180] = 1
+    state[130:140, 190:200] = 1
+    state[130:140, 210:220] = 1
+
+    state[150:160, 50:60] = 1
+    state[150:160, 70:80] = 1
+    state[150:160, 90:100] = 1
+    state[150:160, 110:120] = 1
+    state[150:160, 130:140] = 1
+    state[150:160, 150:160] = 1
+    state[150:160, 170:180] = 1
+    state[150:160, 190:200] = 1
+
+    state[170:180, 70:80] = 1
+    state[170:180, 90:100] = 1
+    state[170:180, 110:120] = 1
+    state[170:180, 130:140] = 1
+    state[170:180, 150:160] = 1
+    state[170:180, 170:180] = 1
+
+    state[190:200, 90:100] = 1
+    state[190:200, 110:120] = 1
+    state[190:200, 130:140] = 1
+    state[190:200, 150:160] = 1
+
+    state[210:220, 110:120] = 1
+    state[210:220, 130:140] = 1
+    return state
 
 
 def main():
 
     default_dims = [250, 250]
-    n_particles = 3500
-    n_steps = 220
+    n_particles = 12000
+    n_steps = 260
     verbose = False
     if '-v' in sys.argv:
         verbose = True
@@ -91,25 +173,39 @@ def main():
         n_particles = int(input('Enter number of particles to use: '))
     if '-nstep' in sys.argv:
         n_steps = int(input('Enter number of steps to use: '))
-
+    start = time.time()
     point_cloud = dict()
 
     # Try box in the center first
     center_x = state.shape[0]/2
     center_y = state.shape[1]/2
-    boxsz = 20
+    boxsz = 10
     state[center_x-boxsz:center_x+boxsz,center_y-boxsz:center_y+boxsz] = 1
 
+    # And add lattice of boxes
+    state = draw_square_lattice(state)
+
     for n_particles in range(n_particles+1):
-        pt = RandomWalker(create_start(10, state, False), n_steps)
+        pt = RandomWalker(create_start(0, state, False), n_steps)
         point_cloud[n_particles] = pt.random_walk
+
     if verbose:
         print "Finished Pre computing "+str(n_steps)+" steps For all "+str(n_particles)+' Particles.'
         print str(len(pt.random_walk)*n_particles)+" Steps Total"
+        t1 = time.time()-start
+        print '[Computation Time: '+str(t1)+' s]\n'
 
-        simulation = pt.run_synchronous_dla(point_cloud, state)
+    simulation = pt.run_synchronous_dla(point_cloud, state, verbose)
+    t2 = (time.time() - start) - t1
+
+    if verbose:
         print "Simulation Complete. Beginning Rendering Process"
-        utility.ImageProcessing.render(simulation, False, 2, False, '')
+        print '[Simulation Time: ' + str(t2) + ' s]'
+    else:
+        plt.imshow(simulation.pop(), 'gray_r')
+        plt.title(str(n_particles) + ' Particle DLA [Final State]')
+        plt.show()
+
 
 if __name__ == '__main__':
     main()
