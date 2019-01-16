bab6c4c71a5ca72bf15efc9f2d279dc2cf3fbae9
TylersDurden <srobbin1@ithaca.edu>
2019-01-15 21:33:15 -0500

    dependencies

--- scripts/linux_libs.sh
+++ scripts/linux_libs.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 echo 'installing libraries'
 pip install numpy
+pip install matplotlib
+pip install scipy
 pip install PyCrypto
 pip install networkx
 pip install resource
 sudo apt-get install jq
 sudo apt-get install tree
 sudo apt-get install ffmpeg
+#EOF
